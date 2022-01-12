06124ef2b1385fd99160f76dac541fd40e85a441
vlakd1059 <49181149+vlakd1059@users.noreply.github.com>
2022-01-12 18:04:29 +0900

    //MessageSystem 예제2

---
+++ LoginCon.java
@@ -0,0 +1,24 @@
+package com.message.service;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/LoginCon")
+public class LoginCon extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		String email = request.getParameter("email");
+		String pw = request.getParameter("pw");
+		
+		
+		
+	}
+
+}
